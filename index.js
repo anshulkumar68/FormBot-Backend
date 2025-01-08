df78c009fdd082174e29abeca0e744ea0f1610b0
Anshul <anshul3071@gmail.com>
2025-01-08 20:12:34 +0530

    feat: udpated port

--- index.js
+++ index.js
@@ -9,16 +9,20 @@ const { error } = require('console')
 const userRoute = require('./routes/formUser')
 const folderRoute = require('./routes/folder');
 const formRoute = require('./routes/forms')
-const PORT = process.env.PORT || 3000 
 
 dotenv.config();
+
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Routes
 app.use('/api/user', userRoute);
 app.use('/api/folder', folderRoute);
 app.use('/api/form', formRoute);
+
+const PORT = process.env.PORT || 4000 
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`);
